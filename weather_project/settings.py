7060baa2cd771969630a9a9487ad0216dba90c73
Rishitha <chowdaryrishitha804@gmail.com>
2025-03-17 20:02:58 +0530

    Updated templates: changed background color and centered content

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -97,10 +97,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'weather_app/static'),
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
