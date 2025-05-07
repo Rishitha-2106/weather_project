2433182969aff4d88ec6bcec9df95fb5c95c28fa
Rishitha <chowdaryrishitha804@gmail.com>
2025-05-07 21:51:25 +0530

    Login and signup modified much better

--- weather_app/models.py
+++ weather_app/models.py
@@ -9,3 +9,12 @@ class UserProfile(models.Model):
     email_id = models.EmailField(max_length=100,default='@gmail.com')
     def __str__(self):
         return self.full_name
+
+
+class SearchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    searched_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.city}"
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.weather_home, name='home'),  # Ensure 'home' is named correctly
     path('food/', views.food_recommendations, name='food_recommendations'),
     path('clothing/', views.clothing_recommendations, name='clothing_recommendations'),
-
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
