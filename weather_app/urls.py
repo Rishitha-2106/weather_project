82b6a38f4e299bbd99a444cc58f2d113b2b45d83
Rishitha <chowdaryrishitha804@gmail.com>
2025-05-07 22:15:37 +0530

    created a profile after logged in

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.user_profile, name='profile'),  # Add profile URL
+
 ]
