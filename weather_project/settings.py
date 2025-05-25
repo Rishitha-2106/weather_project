a49d01d04bc553ee53516fd8ccfa1b5dfd4906fe
Rishitha <chowdaryrishitha804@gmail.com>
2025-05-25 12:15:24 +0530

    initial commit

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -107,3 +107,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+API_KEY = os.getenv("API_KEY")
+
